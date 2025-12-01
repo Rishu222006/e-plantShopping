dcd26af6273d680755ec0bae4202c364f13bd0bd
Rishu222006 <vdkaushik33@gmail.com>
2025-12-01 12:53:23 +0530

    completed tasks on page 2 of assessment

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,33 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const newItem = action.payload;
 
-    
+      const existingItem = state.items.find(item => item.name === newItem.name);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...newItem,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeItem: (state, action) => {
+      const itemName = action.payload;
+      state.name = state.items.filter(item => item.name !== itemName);
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+
+      const item = state.items.find(item => item.name === name);
+
+      if (item) {
+        item.quantity = amount;
+      }
     },
   },
 });
